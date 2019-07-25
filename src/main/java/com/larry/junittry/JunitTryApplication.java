b03c9751464128013c4c5c61b3b1f56ad30871d6
larry <zhongd@aliyun.com>
2019-07-26 00:01:03 +0800

    初始化

---
+++ src/main/java/com/larry/junittry/JunitTryApplication.java
@@ -0,0 +1,13 @@
+package com.larry.junittry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JunitTryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JunitTryApplication.class, args);
+    }
+
+}
